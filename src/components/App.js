4be379fce9d5a7f89623a90c2acbffa915ade5c2
Hrishikesh Shekhar <hrishikeshshekhar@gmail.com>
2020-04-07 01:26:27 +0530

    Add chatbox component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Board from './Board';
 import Slate from './Slate';
 import ScoreBoard from './ScoreBoard';
+import Chatbox from './Chatbox';
 import './App.css';
 
 class App extends React.Component {
@@ -13,16 +14,19 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="slate">
-          <h2> Letters </h2>
-          <Slate></Slate>
           <h2> Score </h2>
           <ScoreBoard></ScoreBoard>
+          <h2> Letters </h2>
+          <Slate></Slate>
         </div>
         <div className="board">
-          <h2> Scrabble app </h2>
+          <h2> Board </h2>
           <Board></Board>
         </div>
-        
+        <div className="chat">
+          <h2> Chatbox </h2>
+          <Chatbox></Chatbox>
+        </div>
       </div>
     );
   }
